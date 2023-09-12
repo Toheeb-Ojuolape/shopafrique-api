31a838d45ed2e5d0ae66f78659f27743384342bb
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-09-12 23:48:08 +0100

    feat:consumed apis for onboarding

--- main.go
+++ main.go
@@ -19,23 +19,20 @@ func init() {
 
 func main() {
 	app := fiber.New()
-	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
-		AllowOrigins:     "*",
-		AllowCredentials: true,
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-	}))
 
-	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+	// change this in production
+	app.Use(cors.New())
+
+	app.Get("/api/healthcheck", func(c *fiber.Ctx) error {
 		return c.Status(http.StatusOK).JSON(fiber.Map{
 			"message": "Everything's ok, stop worrying",
 		})
 	})
 
-	auth := app.Group("auth")
+	auth := app.Group("/api/auth")
 	routes.AuthRoutes(auth)
 
-	user := app.Group("user")
+	user := app.Group("/api/user")
 	routes.UserRoutes(user)
 
 	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
